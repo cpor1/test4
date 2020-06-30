e8a7f958179faf79a4306ce03645a84846162dbd
TUNG <hltrainee@gmail.com>
2020-06-30 09:42:23 +0700

    fix

--- main.go
+++ main.go
@@ -1,51 +1,118 @@
 package main
 
 import (
-	_ "github.com/go-sql-driver/mysql"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"test4/database"
+	"time"
+
+	"github.com/urfave/cli/v2"
 )
 
-func main() {
-	UsingCli()
-	// db.ConnectDb()
-	// now := time.Now().UnixNano()
-	// var err error
-	// engine, err = xorm.NewEngine("mysql", "root:@/m1test?charset=utf8")
-	// fmt.Println(err)
+var db *database.Db = new(database.Db)
 
-	//bai 1: create table using struct
-	//db.CreateTable()
-	// db.Sync2Table()
+func Bai1(c *cli.Context) error {
+	now := time.Now().UnixNano()
+	err := db.ConnectDb()
+	if err != nil {
+		panic(err)
+	}
 
-	//bai 2:
+	err = db.Sync2Table()
+	if err != nil {
+		panic(err)
+	}
 	//insert
-	// user := database.User{"4", "tungedit", 1592456481, now, now}
-	// db.InsertUser(user)
-
-	// update
-	// user := database.User{"5", "tungtest", 11111111110, 159245648121312, now}
-	// db.UpdateUser(user)
-
-	//list
-	// db.ListUser()
-
+	user := database.User{"1", "tungedit", 1592456481, now, now}
+	err = db.InsertUser(user)
+	if err != nil {
+		panic(err)
+	}
+	//update
+	user2 := database.User{"1", "tungtest", 11111111110, 159245648121312, now}
+	err = db.UpdateUser(user2)
+	if err != nil {
+		panic(err)
+	}
+	// list
+	err = db.ListUser()
+	if err != nil {
+		panic(err)
+	}
 	// detail by id
-	// user, _ := db.DetailUser("k1")
-	// fmt.Println(user)
+	user3, err := db.DetailUser("1")
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println(user3)
+	}
 
-	// tạo user thì insert user_id vào user_point với số điểm 10.
-	//db.InsertUserAndPoint(user)
-	// db.ListPoint()
+	//  tạo user thì insert user_id vào user_point với số điểm 10.
+	err = db.InsertUserAndPoint(user)
+	if err != nil {
+		panic(err)
+	}
+	err = db.ListPoint()
+	if err != nil {
+		panic(err)
+	}
+	return nil
+}
 
-	//bai 2
-	// err := db.SessionTest("k5", 1231231)
-	// if err != nil {
-	// 	fmt.Fprintln(nil)
-	// }
-	//bai 3
-	// for i := 0; i < 100; i++ {
-	// 	user := database.User{"k" + strconv.Itoa(i), "user " + strconv.Itoa(i), now, now, now}
-	// 	db.InsertUser(user)
-	// }
-	// db.ScanByRow()
+func Bai2(c *cli.Context) error {
+	err := db.ConnectDb()
+	if err != nil {
+		panic(err)
+	}
 
+	err = db.SessionTest("1", 1231231)
+	if err != nil {
+		panic(err)
+	}
+	return nil
+}
+
+func Bai3(c *cli.Context) error {
+	err := db.ConnectDb()
+	if err != nil {
+		panic(err)
+	}
+
+	now := time.Now().UnixNano()
+	for i := 0; i < 100; i++ {
+		user := database.User{"k" + strconv.Itoa(i), "user " + strconv.Itoa(i), now, now, now}
+		err := db.InsertUser(user)
+		if err != nil {
+			panic(err)
+		}
+	}
+	err = db.ScanByRow()
+	if err != nil {
+		panic(err)
+	}
+	return nil
+}
+
+func main() {
+	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "cli",
+				Usage: "using cli",
+			},
+		},
+	}
+
+	app.Commands = []*cli.Command{
+		{Name: "b1", Usage: "create table, insert, select", Action: Bai1},
+		{Name: "b2", Usage: "update birth then add point", Action: Bai2},
+		{Name: "b3", Usage: "add 100 user then scan row", Action: Bai3},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
